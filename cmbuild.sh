97f30fea9bd90680362ae2127ec5213dd6572512
Tim Wanders <tim241@ziggo.nl>
2017-01-10 18:30:51 +0000

    First commit

---
+++ cmbuild.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+THREADS=6
+echo "#########################"
+echo "   Reversing Patches     "
+echo "#########################"
+for file in sprd-patches/sprd/*.patch ; do
+	patch -R --force --quiet -p1  < $file
+	if [ $? != "0" ]; then
+		echo "Failed to reverse $file"
+	else
+		echo "Successfully reversed $file"
+	fi
+done
+echo "#########################"
+echo "     Syncing repo        "
+echo "#########################"
+repo sync --force-sync -j$THREADS
+echo "#########################"
+echo "    Applying patches"
+echo "#########################"
+for file in sprd-patches/sprd/*.patch ; do
+        patch --force --quiet -p1  < $file
+        if [ $? != "0" ]; then
+		echo "Failed to apply needed patch, try manually patching $file"
+		echo "Reversing failed patch"
+		patch -R --force --quiet -p1  < $file
+		exit
+        else
+                echo "Successfully applied $file"
+        fi
+done
+echo "#########################"
+echo "    Executing source"
+echo "#########################"
+source build/envsetup.sh
+echo "#########################"
+echo "  Executing breakfast"
+echo "#########################"
+breakfast kanas3gnfcxx
+echo "#########################" 
+echo "    Executing make"
+echo "#########################"
+make clean -j$THREADS
+make bacon -j$THREADS
+if [ "$?" = "0" ]; then
+	echo "#########################"
+        echo "     Build Success"
+	echo "#########################"
+
+else
+	echo "#########################"
+	echo "     Build Failed"
+	echo "#########################"
+fi
